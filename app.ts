5cbb460eb6068895aaa27632e36f261bf0ac1838
moses mwangi <moses.mwangi.me@gmail.com>
2024-08-12 14:26:00 +0300

    commit

--- app.ts
+++ app.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import cors from "cors";
+import mimeTypes from "mime-types";
 import usersRoute from "./routes/usersRoute";
 import agentRoute from "./routes/agentRoute";
 import propertyRoute from "./routes/propertyRoute";
 import AppError from "./utils/appError";
 import globalErrorHandler from "./controllers/errorController";
+import path from "path";
 
 const app = express();
 app.use(express.json());
@@ -27,9 +29,32 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
+app.use(
+  "/agents",
+  express.static(path.join(__dirname, "public/agents"), {
+    setHeaders: (res, filePath) => {
+      const mimeType = mimeTypes.lookup(filePath);
+      if (mimeType) {
+        res.setHeader("Content-Type", mimeType);
+      } else {
+        console.warn(`Cannot determine MIME type for file: ${filePath}`);
+      }
+    },
+  })
+);
+
+app.use((req, res, next) => {
+  console.log("Testing middleware");
+  next();
+});
+
 app.use("/api/users", usersRoute);
 app.use("/api/agents", agentRoute);
 app.use("/api/property", propertyRoute);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the API");
+});
 
 app.all("*", (req, res, next) => {
   next(new AppError(`Cant find ${req.originalUrl} on this server`, 500));
