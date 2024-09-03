4ae9f330c32544ecc8e38688515d28ff1a38bacd
moses mwangi <moses.mwangi.me@gmail.com>
2024-09-03 18:34:36 +0300

    commit

--- dist/app.js
+++ dist/app.js
@@ -21,6 +21,7 @@ app.use((0, cookie_parser_1.default)());
 const allowedOrigins = [
     "https://real-estate-mu-peach.vercel.app",
     "https://real-estate-dashboard-kappa.vercel.app",
+    "https://real-estate-dashboard-kappa.vercel.app/",
     "http://localhost:3000",
     "http://localhost:3001",
     "https://real-estate-mu-peach.vercel.app/",
